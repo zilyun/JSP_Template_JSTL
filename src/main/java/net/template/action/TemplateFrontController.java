e5556674ab7266a687430aca7abbe2fd4f05d7fe
DESKTOP-SO3G7VU\jhta <a66074742@gmail.com>
2024-03-11 18:34:00 +0900

    2024-03-11-zihun-03

--- src/main/java/net/template/action/TemplateFrontController.java
+++ src/main/java/net/template/action/TemplateFrontController.java
@@ -11,20 +11,34 @@ import jakarta.servlet.http.HttpServletResponse;
 public class TemplateFrontController extends jakarta.servlet.http.HttpServlet {
 
 	private static final long serialVersionUID = 1L;
-	
+
 	protected void doProcess(HttpServletRequest request, HttpServletResponse response) {
+		/*
+		 * RequestURI = /JSP_Template_JSTL/templatetest.net 
+		 * contextPath =/JSP_Template_JSTL 
+		 * command = /templatetest.net
+		 */
+		String RequestURI = request.getRequestURI();
+		System.out.println(RequestURI);
+		
+		String contextPath = request.getContextPath();
+		System.out.println(contextPath);
+		
+		String command = RequestURI.substring(contextPath.length());
+		System.out.println(command);
 		
 	}
 
 	// doProcess(request, response) 메서드를 구현하여 요청이 GET 방식이든
 	// POST 방식으로 전송되어 오든 같은 메서드에서 요청을 처리할 수 있도록 하였습니다.
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		doProcess(request, response);
 	}
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		doProcess(request, response);
 	}
-
 
 }
